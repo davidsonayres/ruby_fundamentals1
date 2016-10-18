12d6c83e4ee822fc5036e0df53f11576fb152f5d
Michael <mjames.ayres@gmail.com>
2016-10-18 13:21:43 -0400

    Add exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+#How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer
+tip = (55 * 1.20)
+puts tip
+
+#Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+puts "How many fingers am I holding up? Go on, guess! " + ("1").to_s + ", genius."
+
+#Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+puts "I have #{45628*7839} marbles. Don't be jealous."
+
+#What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+#true
